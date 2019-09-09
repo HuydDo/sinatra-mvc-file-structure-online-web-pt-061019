9920fc46ee596cd7c80e1e079c7dceda8c855f3a
Huy Do <huyddo40@gmail.com>
2019-09-09 22:22:52 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,12 +1,13 @@
 class Dog
   attr_accessor :name, :breed, :age
   @all = []
+  
   def initial(name, breed, age)
     @name = name
     @breed = breed
     @age = age
   end
-  end
+  
   def all
     @all
   end
